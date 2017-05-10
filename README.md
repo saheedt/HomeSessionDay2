b454945b7275d4b04967b95bdd4bbeebba8eadc6
saheedt <stunnafrsh007@gmail.com>
2017-05-10 14:16:49 +0100

    Add updated teamnews app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HomeSessionDay2
+
+* ## TeamNews:
+		A commandline application that consumes a public api which fetches team list of a football team.
+
+	* ## Usage:
+		* ## $ teamnews --for [team_name]:
+			*Returns the entire team list*
---
+++ TeamNews/app/teamnews.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+//request commandline arguement parser and http client.
+let argv = require('yargs').argv;
+let request = require('request');
+
+//Make sure argument is a string.
+if(typeof argv.for != "string"){
+	console.log("Only string allowed");
+	return;
+}
+
+//Hardcode team id to search for [arsenal or man united]. 
+let team = (argv.for == 'Arsenal' || argv.for == 'arsenal' ) ? 57 : 66 ;
+
+//http rquest options.
+let options = {
+	url: 'http://api.football-data.org/v1/teams/'+ team +'/players',
+};
+
+//Callback function, where http statusCode & error is checked and
+//return data is parsed.
+const callback = (error, response, body) => {
+	console.log(error);
+	console.log(response);
+	if(!error && response.statusCode == 200){
+		let data = JSON.parse(body);
+		console.log(data.players);
+	}
+	
+};
+
+//The http request
+request(options, callback);
